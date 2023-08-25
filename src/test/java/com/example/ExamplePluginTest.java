2a740d28e92409095d4fcb72b4caf02acd718b49
SkylerPIlot <SkylerParthemore@gmail.com>
2023-08-25 11:20:46 -0700

    Adds Ethan's Auto Addnode grabbing functionality

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -5,12 +5,13 @@ import com.example.EthanApiPlugin.EthanApiPlugin;
 
 import com.example.PacketUtils.PacketUtilsPlugin;
 
+import com.example.testerplugin.testerplugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class ExamplePluginTest {
     public static void main(String[] args) throws Exception {
-        ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, PacketUtilsPlugin.class);
+        ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, PacketUtilsPlugin.class, testerplugin.class);
         RuneLite.main(args);
     }
 }
