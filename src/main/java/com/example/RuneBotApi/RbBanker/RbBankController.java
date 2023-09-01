66e778e80aa2f77cc453155cb18844c30a068726
xylphie <nanabread@protonmail.com>
2023-09-01 13:00:45 -0500

    Users don't need to see config that's still in development

--- src/main/java/com/example/RuneBotApi/RbBanker/RbBankController.java
+++ src/main/java/com/example/RuneBotApi/RbBanker/RbBankController.java
@@ -33,124 +33,124 @@ import java.util.Optional;
         tags = {"Banking"}
 )
 public class RbBankController extends Plugin {
-
-    protected int timeout = 0;
-    protected int errTimer = 0;
-    private State state;
-    protected RbBankConfig config = RBApi.getConfigManager().getConfig(RbBankConfig.class);
-    @Provides
-    public RbBankConfig getConfig(ConfigManager configManager) {
-        return config;
-    }
-    protected Client client = RBApi.getClient();
-    private ResupplyController resupplyController;
-
-
-    protected boolean sellItems = false;
-    protected boolean buySuppliesIfNeeded = true;
-
-    //var for if we want to sell items at [config] hours
-
-
-    public RbBankController()
-    {
-//        this.resupplyController = new ResupplyController();
-        state = State.OPEN_BANK;
-    }
-
-    public boolean eventLoop() {
-        if (0 < timeout--) return true; // only exec if no timeout
-
-
-        switch (state)
-        {
-            case TELEPORT:
-                if (Inventory.search().withId(RBConstants.houseTabId).result().isEmpty())
-                    throw new NoSuchInventoryItemException("No house tabs found in inventory.");
-                InventoryInteraction.useItem(RBConstants.houseTabId, "Break");
-                timeout = 8;
-                state = State.DOOR_STUCK;
-            break; case USE_POOL:
-                Optional<TileObject> pool = TileObjects.search().withAction("Drink").first();
-                if (pool.isPresent()) {
-                    TileObjectInteraction.interact(pool.get(), "Drink");
-                    state = State.AWAIT_POOL;
-                    return true;
-                } else {
-                    state = State.DOOR_STUCK;
-                }
-            break; case AWAIT_POOL:
-                if (!EthanApiPlugin.isMoving()) state = State.DOOR_STUCK;
-            break; case DOOR_STUCK:
-                Optional<TileObject> teleportObject = getTeleportObject();
-                if (teleportObject.isPresent()) {
-                    state = exitIfPathable(teleportObject.get()); // EXIT_POH on success
-                } else {
-                    state = State.FAILURE;
-                    throw new NoSuchGameObjectException("Object '" + config.bankingLocation().getBankingData().getTeleportObject() + "' does not exist in the current scene.");
-                }
-            break; case EXIT_POH:
-                if (LocationInformation.getMapSquareId() != config.bankingLocation().getBankingData().getLocationId()) {
-                    if (errTimer++ > 20) {
-                        state = State.DOOR_STUCK;
-                        errTimer = 0;
-                    }
-                    return true;
-                }
-                state = State.OPEN_BANK;
-            break; case OPEN_BANK:
-                try {
-                    if (!Banks.openNearestBank()) state = State.BANK_MANAGER;
-                } catch (Exception e) {
-                    RBApi.panic();
-                }
-            break; case BANK_MANAGER:
-                return resupplyController.eventLoop(); // when this returns false we return execution to the yielding class
-
-        }
-        return true;
-    }
-
-    private State exitIfPathable(TileObject obj)
-    {
-        // we need to have LoS to at least 2 orthogonal tiles otherwise we could end up on the other side of a wall
-        // finding pathable tiles to object.getWorldLocation doesn't work since the destination resides within the obj
-        int pathable = 0;
-        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dx(1)).isReachable() ? 1 : 0;
-        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dx(-1)).isReachable() ? 1 : 0;
-        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dy(1)).isReachable() ? 1 : 0;
-        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dy(-1)).isReachable() ? 1 : 0;
-
-        if (pathable > 1) {
-            TileObjectInteraction.interact(obj, config.bankingLocation().getBankingData().getAction());
-            return State.EXIT_POH;
-        } else {
-            state = State.FAILURE;
-            throw new NoWalkablePathException("Cannot path to object '" + config.bankingLocation().getBankingData().getTeleportObject() + "'.");
-        }
-    }
-
-    private Optional<TileObject> getTeleportObject()
-    {
-        Optional<TileObject> bankingObject;
-        if (config.bankingLocation() == RbBankConfig.BankingLocation.GRAND_EXCHANGE) {
-            bankingObject = TileObjects.search().withId(13615).first();
-        } else {
-            bankingObject = TileObjects.search().withName(config.bankingLocation().getBankingData().getTeleportObject()).first();
-        }
-        return bankingObject;
-    }
-
-    private enum State
-    {
-        TELEPORT,
-        USE_POOL,
-        AWAIT_POOL,
-        DOOR_STUCK,
-        EXIT_POH,
-        OPEN_BANK,
-        BANK_MANAGER,
-        FAILURE // implement failure handler that logs user out and prints the reason in the log file
-    }
+//
+//    protected int timeout = 0;
+//    protected int errTimer = 0;
+//    private State state;
+//    protected RbBankConfig config = RBApi.getConfigManager().getConfig(RbBankConfig.class);
+//    @Provides
+//    public RbBankConfig getConfig(ConfigManager configManager) {
+//        return config;
+//    }
+//    protected Client client = RBApi.getClient();
+//    private ResupplyController resupplyController;
+//
+//
+//    protected boolean sellItems = false;
+//    protected boolean buySuppliesIfNeeded = true;
+//
+//    //var for if we want to sell items at [config] hours
+//
+//
+//    public RbBankController()
+//    {
+////        this.resupplyController = new ResupplyController();
+//        state = State.OPEN_BANK;
+//    }
+//
+//    public boolean eventLoop() {
+//        if (0 < timeout--) return true; // only exec if no timeout
+//
+//
+//        switch (state)
+//        {
+//            case TELEPORT:
+//                if (Inventory.search().withId(RBConstants.houseTabId).result().isEmpty())
+//                    throw new NoSuchInventoryItemException("No house tabs found in inventory.");
+//                InventoryInteraction.useItem(RBConstants.houseTabId, "Break");
+//                timeout = 8;
+//                state = State.DOOR_STUCK;
+//            break; case USE_POOL:
+//                Optional<TileObject> pool = TileObjects.search().withAction("Drink").first();
+//                if (pool.isPresent()) {
+//                    TileObjectInteraction.interact(pool.get(), "Drink");
+//                    state = State.AWAIT_POOL;
+//                    return true;
+//                } else {
+//                    state = State.DOOR_STUCK;
+//                }
+//            break; case AWAIT_POOL:
+//                if (!EthanApiPlugin.isMoving()) state = State.DOOR_STUCK;
+//            break; case DOOR_STUCK:
+//                Optional<TileObject> teleportObject = getTeleportObject();
+//                if (teleportObject.isPresent()) {
+//                    state = exitIfPathable(teleportObject.get()); // EXIT_POH on success
+//                } else {
+//                    state = State.FAILURE;
+//                    throw new NoSuchGameObjectException("Object '" + config.bankingLocation().getBankingData().getTeleportObject() + "' does not exist in the current scene.");
+//                }
+//            break; case EXIT_POH:
+//                if (LocationInformation.getMapSquareId() != config.bankingLocation().getBankingData().getLocationId()) {
+//                    if (errTimer++ > 20) {
+//                        state = State.DOOR_STUCK;
+//                        errTimer = 0;
+//                    }
+//                    return true;
+//                }
+//                state = State.OPEN_BANK;
+//            break; case OPEN_BANK:
+//                try {
+//                    if (!Banks.openNearestBank()) state = State.BANK_MANAGER;
+//                } catch (Exception e) {
+//                    RBApi.panic();
+//                }
+//            break; case BANK_MANAGER:
+//                return resupplyController.eventLoop(); // when this returns false we return execution to the yielding class
+//
+//        }
+//        return true;
+//    }
+//
+//    private State exitIfPathable(TileObject obj)
+//    {
+//        // we need to have LoS to at least 2 orthogonal tiles otherwise we could end up on the other side of a wall
+//        // finding pathable tiles to object.getWorldLocation doesn't work since the destination resides within the obj
+//        int pathable = 0;
+//        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dx(1)).isReachable() ? 1 : 0;
+//        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dx(-1)).isReachable() ? 1 : 0;
+//        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dy(1)).isReachable() ? 1 : 0;
+//        pathable += EthanApiPlugin.canPathToTile(obj.getWorldLocation().dy(-1)).isReachable() ? 1 : 0;
+//
+//        if (pathable > 1) {
+//            TileObjectInteraction.interact(obj, config.bankingLocation().getBankingData().getAction());
+//            return State.EXIT_POH;
+//        } else {
+//            state = State.FAILURE;
+//            throw new NoWalkablePathException("Cannot path to object '" + config.bankingLocation().getBankingData().getTeleportObject() + "'.");
+//        }
+//    }
+//
+//    private Optional<TileObject> getTeleportObject()
+//    {
+//        Optional<TileObject> bankingObject;
+//        if (config.bankingLocation() == RbBankConfig.BankingLocation.GRAND_EXCHANGE) {
+//            bankingObject = TileObjects.search().withId(13615).first();
+//        } else {
+//            bankingObject = TileObjects.search().withName(config.bankingLocation().getBankingData().getTeleportObject()).first();
+//        }
+//        return bankingObject;
+//    }
+//
+//    private enum State
+//    {
+//        TELEPORT,
+//        USE_POOL,
+//        AWAIT_POOL,
+//        DOOR_STUCK,
+//        EXIT_POH,
+//        OPEN_BANK,
+//        BANK_MANAGER,
+//        FAILURE // implement failure handler that logs user out and prints the reason in the log file
+//    }
 
 }
