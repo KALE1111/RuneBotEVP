3769d75182837422897aea1bfb522265b27ce18b
Dan Bloodworth <43654582+DBloodworth95@users.noreply.github.com>
2024-02-04 23:39:35 +0000

    Update BankInventory.java

--- src/main/java/com/example/EthanApiPlugin/Collections/BankInventory.java
+++ src/main/java/com/example/EthanApiPlugin/Collections/BankInventory.java
@@ -24,7 +24,6 @@ public class BankInventory {
     static List<Widget> bankInventoryItems = new ArrayList<>();
 
     public static ItemQuery search() {
-        RetryCollection();
         return new ItemQuery(bankInventoryItems.stream().filter(Objects::nonNull).collect(Collectors.toList()));
     }
 
